560071835cf9ddb3c7b1a7101f1c7d1b08fa2bc0
artav23 <artieda.avery@outlook.com>
2025-12-15 21:55:42 -0500

    Update restAPI_dev.js

--- restAPI_dev.js
+++ restAPI_dev.js
@@ -6,9 +6,4 @@ app.use('/api', api_routes);
 
 app.use('/', express.static('front_end'));
 
-app.listen(3000, function() {
-    console.log("Server is running.")
-    console.log(employee);
-});
-
 module.exports = app;
